536f4a3fc1ce6ac249fef4e53d49761dd4a4b59a
piiito <birgittevedaa@hotmail.com>
2022-11-06 23:39:58 +0100

    Files been cleaned up by prettier

--- js/jokertest.js
+++ js/jokertest.js
@@ -1,4 +1,4 @@
-import { dadJoke } from "./joker.js";
+import { dadJoke } from './joker.js';
 
 export async function testDadJoke() {
   const joke = await dadJoke();
@@ -9,3 +9,5 @@ export async function testDadJoke() {
 }
 
 testDadJoke();
+
+('jes');
